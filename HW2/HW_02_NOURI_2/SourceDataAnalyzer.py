1d225b900781b51b897d29286268702f722b603a
Ebrahim Nouri <viroos@live.com>
2025-11-02 13:59:08 +0330

    HW2

--- HW2/HW_02_NOURI_2/SourceDataAnalyzer.py
+++ HW2/HW_02_NOURI_2/SourceDataAnalyzer.py
@@ -4,31 +4,30 @@ class Product:
         self.price = price
         self.quantity = quantity
 
-
 with open("sales.txt.txt", "r") as f:
     lines = f.readlines()
 
 products = []
 
 for line in lines:
-    line = line.replace("\n", "")
+    line = line.strip()
     split = line.split(",")
     product = Product(split[0], float(split[1]), int(split[2]))
     products.append(product)
 
-count_of_prod = 0
 count_of_transactions = len(products)
-some_of_sold = 0
-most_of_sold = 0
-best_selling_product = max(products, key=lambda pd: pd.quantity)
+total_sold = 0
+count_of_prod = 0
 
 for product in products:
-    count_of_prod += product.quantity
-    some_of_sold += product.price
-average = some_of_sold / count_of_transactions
+    total_sold += product.price * product.quantity
+    count_of_prod += product.quantity# مجموع تعداد کالا
 
-print("تعداد ترکنش ها: ", count_of_transactions)
-print("مجموع فروش: %d دلار" % count_of_transactions)
+best_selling_product = max(products, key=lambda pd: pd.quantity)  # پرفروش‌ترین کالا
+average = total_sold / count_of_transactions
+
+print("تعداد تراکنش ها:", count_of_transactions)
+print("مجموع فروش: %d دلار" % total_sold)
 print("پرفروش ترین کالا: %s (فروش %d عدد)" % (best_selling_product.product_name, best_selling_product.quantity))
-print(f"میانگین مبلغ خرید: {average * 100:.2f}% دلار")
-
+print(f"میانگین مبلغ خرید: {average:.2f} دلار")
+print("مجموع کل کالاهای فروخته شده:", count_of_prod)
